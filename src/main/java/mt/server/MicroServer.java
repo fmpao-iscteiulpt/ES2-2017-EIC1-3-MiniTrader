25c12fb61cfee9fd2bc4c65bb100cbe3d69daf09
Frsncisco <Frsncisco@Francisco-PC>
2017-05-10 17:29:27 +0100

    added xml file on one project

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -1,5 +1,7 @@
 package mt.server;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,6 +14,23 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import mt.Order;
 import mt.comm.ServerComm;
@@ -254,8 +273,78 @@ public class MicroServer implements MicroTraderServer {
 		
 		//save order on map
 		Set<Order> orders = orderMap.get(o.getNickname());
-		orders.add(o);		
+		orders.add(o);
+		putInXML(o);
 	}
+	
+	private void putInXML(Order orderXML){
+		 try {	
+	         File inputFile = new File("MicroTraderPersistence.xml");
+	         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	         Document doc = dBuilder.parse(inputFile);
+	         doc.getDocumentElement().normalize();         
+	         NodeList nList = doc.getElementsByTagName("Order");
+	         System.out.println("----- Navigate the tree nodes -----");
+	         for (int temp = 0; temp < nList.getLength(); temp++) {
+	            Node nNode = nList.item(temp);
+	            System.out.print(nNode.getNodeName() + " ");
+	            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+	               Element eElement = (Element) nNode;
+	               System.out.print("Id:" + eElement.getAttribute("Id"));
+	               System.out.print(" Type:" + eElement.getAttribute("Type"));
+	               System.out.print(" Stock:" + eElement.getAttribute("Stock"));
+	               System.out.print(" Units:" + eElement.getAttribute("Units"));
+	               System.out.print(" Price:" + eElement.getAttribute("Price"));
+	               System.out.println();
+	            }
+	         }
+	         System.out.println("----- Search the tree with xpath queries -----");  
+	         // Query 1 
+	         XPathFactory xpathFactory = XPathFactory.newInstance();
+	         XPath xpath = xpathFactory.newXPath();
+	         XPathExpression expr = xpath.compile("/XML/Order[@Id='2']/@*");
+	         NodeList nl = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+	         System.out.print("Order ");
+	         for (int i = 0; i < nl.getLength(); i++) {
+	             System.out.print(nl.item(i).getNodeName()  + ":");
+	             System.out.print(nl.item(i).getFirstChild().getNodeValue()  + " ");
+	         }
+	         // Query 2
+	         expr = xpath.compile("/XML/Order[@Id='2']/Customer");
+	         String str = (String) expr.evaluate(doc, XPathConstants.STRING);
+	         System.out.println();System.out.println("Customer of Order Id=5: " + str);
+	         
+	         // Create new element Order with attributes
+	         Element newElement = doc.createElement("Order");
+	         newElement.setAttribute("Id", Integer.toString(orderXML.getServerOrderID()));
+	         if(orderXML.isBuyOrder()==true){
+	        	 newElement.setAttribute("Type", "Buy");
+	         }
+	         else if(orderXML.isSellOrder()==true){
+	        	 newElement.setAttribute("Type", "Sell");
+	         }
+	         
+	         newElement.setAttribute("Stock", orderXML.getStock());
+	         newElement.setAttribute("Units", (Integer.toString(orderXML.getNumberOfUnits())));
+	         newElement.setAttribute("Price", (Integer.toString((int) orderXML.getPricePerUnit())));
+	  
+	         // Add new node to XML document root element
+	         System.out.println("----- Adding new element to root element -----");
+	         System.out.println("Root element :" + doc.getDocumentElement().getNodeName());         
+	         System.out.println("Add Order Id='5' Type='Buy' Stock='PT' Units='15' Price='20'");
+	         Node n = doc.getDocumentElement();
+	         n.appendChild(newElement);
+	         // Save XML document
+	         System.out.println("Save XML document.");
+	         Transformer transformer = TransformerFactory.newInstance().newTransformer();
+	         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	         StreamResult result = new StreamResult(new FileOutputStream("MicroTraderPersistence.xml"));
+	         DOMSource source = new DOMSource(doc);
+	         transformer.transform(source, result);
+	      } catch (Exception e) { e.printStackTrace(); }
+	   }
+	
 
 	/**
 	 * Process the sell order
